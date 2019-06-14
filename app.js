5a09a2f497129d028d9369360c018e0c5413fe7c
Frank Li <b439988l@gmail.com>
2019-06-15 00:31:14 +1000

    db connection good

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-var createError = require('http-errors');
+//Modules
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+//db connection
+mongoose.connect(process.env.DB_CON, ()=> console.log('MongoDB Atlas connection successful'));
+
+//Routes
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
+//Express Server
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.listen(3000, () => console.log('ThreePointSix API listening on port 3000'))
 module.exports = app;
